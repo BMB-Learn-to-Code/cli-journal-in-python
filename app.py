aea68d4e1d68d512fba6dc84f8d0b979fe935350
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2025-07-18 00:24:51 +0200

    Switch journal storage to SQLite database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from database import add_items, view_items
+from database import add_items, view_items, close_connection, create_db
 
 # Create the menu variable with the prompt content (print the menu)
 # # The prompt should contemplate three options, create a new entry (1), list all entries (2), exit the app (3)
@@ -9,8 +9,11 @@ menu = """Please select one of the following tasks
 
 # Create a welcome variable to store the alternatice welcome message
 welcome = "Welcome to your daily CLI journal"
-# Initiate the REPL  that collects each time the task to be executed (a while loop, if the user types tree it breaks the loop)
 
+# Create DB
+create_db()
+
+# Initiate the REPL  that collects each time the task to be executed (a while loop, if the user types tree it breaks the loop)
 print(welcome)
 while (user_input := input(menu)) != "3":
    if user_input == "1":
@@ -21,3 +24,5 @@ while (user_input := input(menu)) != "3":
        view_items()
    else:
        print("Invalid option please chose an option from 1 to 3")
+
+close_connection()
--- database.py
+++ database.py
@@ -1,12 +1,25 @@
+import sqlite3
 import datetime
-entries = []
 
+connection  = sqlite3.connect("data.db")
+
+
+def create_db():
+    with connection:
+        connection.execute("CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);")
+
+def close_connection():
+    connection.close()
 
 def add_items():
     new_story = input("Enter your entry for today:\n")
     new_date = datetime.datetime.now()
-    entries.append({"content": new_story, "date": new_date })
+    with connection:
+        connection.execute("INSERT INTO entries VALUES(?,?)", (new_story, new_date))
 
 def view_items():
+    entries = []
+    with connection:
+        entries = connection.execute("SELECT * FROM entries").fetchall()
     for entry in entries:
-        print(f"{entry['date'].strftime('%Y-%m-%d')} - {entry['content']}\n")
+        print(f"{entry[1]} - {entry[0]}\n")
